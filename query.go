960eba1a01526c55d26ace84ad080fa8b7a3468e
scottcagno <scottiecagno@gmail.com>
2015-11-20 17:42:46 -0500

    working on query engine

--- query.go
+++ query.go
@@ -1,5 +1,7 @@
 package dbdb
 
+import "fmt"
+
 type QueryComp interface {
 	Comp(v interface{}) bool
 	Field() string
@@ -11,7 +13,7 @@ type Eq struct {
 }
 
 func (c Eq) Comp(v interface{}) bool {
-	return v == c.Val
+	return fmt.Sprint(v) == fmt.Sprint(c.Val)
 }
 
 func (c Eq) Field() string {
@@ -24,7 +26,7 @@ type Ne struct {
 }
 
 func (c Ne) Comp(v interface{}) bool {
-	return v != c.Val
+	return fmt.Sprint(v) != fmt.Sprint(c.Val)
 }
 
 func (c Ne) Field() string {
@@ -37,8 +39,39 @@ type Gt struct {
 }
 
 func (c Gt) Comp(v interface{}) bool {
-	return false
-	//return v > c.Val
+	var dbVal float64
+	switch v.(type) {
+	case float64:
+		dbVal = v.(float64)
+	default:
+		return false
+	}
+	switch c.Val.(type) {
+	case int:
+		return dbVal > float64(c.Val.(int))
+	case int8:
+		return dbVal > float64(c.Val.(int8))
+	case int16:
+		return dbVal > float64(c.Val.(int16))
+	case int32:
+		return dbVal > float64(c.Val.(int32))
+	case int64:
+		return dbVal > float64(c.Val.(int64))
+	case uint:
+		return dbVal > float64(c.Val.(uint))
+	case uint8:
+		return dbVal > float64(c.Val.(uint8))
+	case uint16:
+		return dbVal > float64(c.Val.(uint16))
+	case uint32:
+		return dbVal > float64(c.Val.(uint32))
+	case uint64:
+		return dbVal > float64(c.Val.(uint64))
+	case float32:
+		return dbVal > float64(c.Val.(float32))
+	default:
+		return false
+	}
 }
 
 func (c Gt) Field() string {
